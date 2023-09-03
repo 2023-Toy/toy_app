29c75fa5412686b575860cbdca686840fa684b0b
김경호 <kkh9331@naver.com>
2023-09-03 23:50:08 +0900

    transaction write xml

--- app/src/main/java/com/opensw/toy/fragment/TransactionWriteFragment.kt
+++ app/src/main/java/com/opensw/toy/fragment/TransactionWriteFragment.kt
@@ -1,8 +1,10 @@
 package com.opensw.toy.fragment
 
 
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipDrawable
 import com.opensw.toy.R
 import com.opensw.toy.base.BaseFragment
 import com.opensw.toy.databinding.FragmentTransactionWriteBinding
@@ -11,19 +13,24 @@ import com.opensw.toy.util.SubCategory
 class TransactionWriteFragment : BaseFragment<FragmentTransactionWriteBinding>(R.layout.fragment_transaction_write) {
     override fun initView() {
         binding.apply {
+            btTransactionWriteAdd.setOnClickListener {
+                Toast.makeText(context, "글 등록하기", Toast.LENGTH_SHORT).show()
+            }
             tbTransactionWrite.ibToolbarBack.setOnClickListener { findNavController().popBackStack() }
             chipToy.setOnClickListener {
+                scSubcategory.scrollTo(0,0)
                 cgTransactionWriteSubCategory.removeAllViews()
                 SubCategory.Toy.forEach{ it ->
-                    cgTransactionWriteSubCategory.addView(Chip(context).apply {
+                    cgTransactionWriteSubCategory.addView((layoutInflater.inflate(R.layout.chip_pink,cgTransactionWriteSubCategory,false) as Chip).apply {
                         text = it
                     })
                 }
             }
             chipBaby.setOnClickListener {
+                scSubcategory.scrollTo(0,0)
                 cgTransactionWriteSubCategory.removeAllViews()
                 SubCategory.Baby.forEach{ it ->
-                    cgTransactionWriteSubCategory.addView(Chip(context).apply {
+                    cgTransactionWriteSubCategory.addView((layoutInflater.inflate(R.layout.chip_pink,cgTransactionWriteSubCategory,false) as Chip).apply {
                         text = it
                     })
                 }
