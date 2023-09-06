2b126f5b405664343b4d98f0b6f8cf5ade61356b
kguard <>
2023-09-06 17:34:19 +0900

    Revert "openSource License 추가"
    This reverts commit 50c6b2880461751c36ccbcce51368291cdcb1be9.

--- app/src/main/java/com/opensw/toy/fragment/HomeFragment.kt
+++ app/src/main/java/com/opensw/toy/fragment/HomeFragment.kt
@@ -1,7 +1,5 @@
 package com.opensw.toy.fragment
 
-import android.content.Intent
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.opensw.toy.R
 import com.opensw.toy.base.BaseFragment
 import com.opensw.toy.databinding.FragmentHomeBinding
@@ -22,15 +20,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override fun initListener() {
         //TODO("Not yet implemented")
-        binding.apply {
-            tvAppName.setOnLongClickListener {
-                Intent(context, OssLicensesMenuActivity::class.java).also {
-                    OssLicensesMenuActivity.setActivityTitle("오픈소스 라이선스")
-                    startActivity(it)
-                }
-                return@setOnLongClickListener false
-            }
-        }
     }
 
 }
